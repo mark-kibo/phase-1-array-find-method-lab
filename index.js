85408e854d03b5d3d9f0a1f8f4d2b59527fb2cb4
mark-kibo <kibochamark@gmail.com>
2023-07-14 09:13:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(records){
+    const result =records.find(item => item.result === "W")
+    
+    if (result == null ){
+        return undefined
+    }else{
+        return result.year;
+    }
+}
